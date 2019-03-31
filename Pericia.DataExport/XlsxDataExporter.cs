125fc67ef50724dd13fd4406e7ddcc1c42c3f2c9
Guillaume Lacasa <github@adhess.net>
2019-03-31 13:44:30 +0200

    Correct date cells

--- Pericia.DataExport/XlsxDataExporter.cs
+++ Pericia.DataExport/XlsxDataExporter.cs
@@ -22,6 +22,8 @@ namespace Pericia.DataExport
         Sheets sheets;
         uint sheetCount = 0;
 
+        private Lazy<uint> dateStyleIndex;
+
         public XlsxDataExporter()
         {
             package = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook);
@@ -40,6 +42,73 @@ namespace Pericia.DataExport
             sheets = new Sheets();
             workbook.Append(sheets);
 
+            dateStyleIndex = new Lazy<uint>(GenerateStylesheet);
+        }
+
+        private uint GenerateStylesheet()
+        {
+            WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet();
+            WorkbookStylesPart workbookStylesPart1 = workbookPart.AddNewPart<WorkbookStylesPart>();
+            Stylesheet stylesheet1 = new Stylesheet() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "x14ac x16r2 xr" } };
+            stylesheet1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            stylesheet1.AddNamespaceDeclaration("x14ac", "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac");
+            stylesheet1.AddNamespaceDeclaration("x16r2", "http://schemas.microsoft.com/office/spreadsheetml/2015/02/main");
+            stylesheet1.AddNamespaceDeclaration("xr", "http://schemas.microsoft.com/office/spreadsheetml/2014/revision");
+
+            Fonts fonts1 = new Fonts() { Count = 1U, KnownFonts = true };
+            Font font1 = new Font();
+            FontSize fontSize1 = new FontSize() { Val = 11D };
+            Color color1 = new Color() { Theme = 1U };
+            FontName fontName1 = new FontName() { Val = "Calibri" };
+            FontFamilyNumbering fontFamilyNumbering1 = new FontFamilyNumbering() { Val = 2 };
+            FontScheme fontScheme1 = new FontScheme() { Val = FontSchemeValues.Minor };
+            font1.Append(fontSize1);
+            font1.Append(color1);
+            font1.Append(fontName1);
+            font1.Append(fontFamilyNumbering1);
+            font1.Append(fontScheme1);
+            fonts1.Append(font1);
+
+            Fills fills1 = new Fills() { Count = 1U };
+            Fill fill1 = new Fill();
+            PatternFill patternFill1 = new PatternFill() { PatternType = PatternValues.None };
+            fill1.Append(patternFill1);
+            fills1.Append(fill1);
+
+            Borders borders1 = new Borders() { Count = 0U };
+            Border border1 = new Border();
+            LeftBorder leftBorder1 = new LeftBorder();
+            RightBorder rightBorder1 = new RightBorder();
+            TopBorder topBorder1 = new TopBorder();
+            BottomBorder bottomBorder1 = new BottomBorder();
+            DiagonalBorder diagonalBorder1 = new DiagonalBorder();
+            border1.Append(leftBorder1);
+            border1.Append(rightBorder1);
+            border1.Append(topBorder1);
+            border1.Append(bottomBorder1);
+            border1.Append(diagonalBorder1);
+            borders1.Append(border1);
+
+            CellStyleFormats cellStyleFormats1 = new CellStyleFormats() { Count = 1U };
+            CellFormat cellFormat1 = new CellFormat() { NumberFormatId = 0U };
+            cellStyleFormats1.Append(cellFormat1);
+
+            CellFormats cellFormats1 = new CellFormats() { Count = 2U };
+            CellFormat cellFormat43 = new CellFormat() { NumberFormatId = 0U, FormatId = 0U };
+            CellFormat cellFormat44 = new CellFormat() { NumberFormatId = 14U, FormatId = 0U, ApplyNumberFormat = true };
+
+            cellFormats1.Append(cellFormat43);
+            cellFormats1.Append(cellFormat44);
+
+            stylesheet1.Append(fonts1);
+            stylesheet1.Append(fills1);
+            stylesheet1.Append(borders1);
+            stylesheet1.Append(cellStyleFormats1);
+            stylesheet1.Append(cellFormats1);
+
+            workbookStylesPart1.Stylesheet = stylesheet1;
+            return 1;
         }
 
         protected override void NewSheet(string name)
@@ -61,7 +130,7 @@ namespace Pericia.DataExport
 
             NewLine();
         }
-        
+
         protected override void NewLine()
         {
             currentCol = 1;
@@ -87,12 +156,9 @@ namespace Pericia.DataExport
             }
             else if (data is DateTime)
             {
-                // TODO date style
-                cell.DataType = CellValues.String;
-                //cell.StyleIndex = 1U;
-                var dateTime = (DateTime)data;
-                //textValue = dateTime.ToString("s", CultureInfo.InvariantCulture);
-                cell.CellValue = new CellValue(dateTime.ToShortDateString());
+                cell.DataType = CellValues.Date;
+                cell.StyleIndex = dateStyleIndex.Value;
+                cell.CellValue = new CellValue(((DateTime)data).ToString("s", CultureInfo.InvariantCulture));
             }
             else if (data is bool)
             {
@@ -139,7 +205,7 @@ namespace Pericia.DataExport
         }
 
 
-        
+
         private List<string> sheetNames = new List<string>();
         protected string NewSheetName(string suggestedName)
         {
